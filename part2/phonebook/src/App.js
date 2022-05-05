1ab1fd2e1958d0ad28943c5ae9055071006d550a
songmeo <songmeo@pm.me>
2022-05-05 10:57:24 +0300

    2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Person from './Person'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
@@ -7,14 +8,21 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterValue, setNewFilterValue] = useState('')
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }  
-  ])
+  const [persons, setPersons] = useState([])
   const [personsToShow, setPersonsToShow] = useState(persons)
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+    })
+  }, [])
+
+  useEffect(() => {
+    setPersonsToShow(persons.filter(person => person.name.toLowerCase().startsWith(filterValue.toLowerCase())))
+  }, [persons, filterValue])
+  
   const addPerson = (event) => {
     event.preventDefault()
     const person = {
@@ -42,7 +50,6 @@ const App = () => {
 
   const handleFilter = (event) => {
     setNewFilterValue(event.target.value)
-    setPersonsToShow(persons.filter(person => person.name.toLowerCase().startsWith(event.target.value.toLowerCase())))
   }
 
   return (
