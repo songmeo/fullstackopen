700b621b62f1dbc77a38d61f069100af433780d2
songmeo <songmeo@pm.me>
2022-06-22 10:37:16 +0300

    2.13 countryToShow state update doesnt trigger rerender

--- part2/countries/src/Country.js
+++ part2/countries/src/Country.js
@@ -4,7 +4,14 @@ import axios from 'axios'
 const Country = (props) => {
     const countriesLength = props.countriesToShow.length
     const [countryToShow, setCountryToShow] = useState([])
-
+    const getCountryDetails = (countryToShowName) => {
+        axios
+        .get(`https://restcountries.com/v3.1/name/${countryToShowName}`)
+        .then(response => {             
+            setCountryToShow(response.data)
+        })
+    }
+    
     if (countriesLength > 10) {
         return (
             <p>Too many matches, specify another filter</p>
@@ -12,14 +19,7 @@ const Country = (props) => {
     }
     else if (countriesLength === 1 && countryToShow.length === 0) {
         const countryToShowName = props.countriesToShow[0].name.common
-        axios
-        .get(`https://restcountries.com/v3.1/name/${countryToShowName}`)
-        .then(response => {
-            setCountryToShow(response.data)
-        })
-        return (
-            <p>loading</p>
-        )
+        return getCountryDetails(countryToShowName)
     }
     else if (countriesLength === 1 && countryToShow.length === 1) {
         const country = countryToShow[0]
@@ -40,9 +40,30 @@ const Country = (props) => {
     }
     else {
         return (
-            props.countriesToShow.map(country => <ul key={country.name.common}>{country.name.common}</ul>)
-        )
-    }
+            <div>
+                { props.countriesToShow.map(country => {
+                    return (
+                        <ul key={country.name.common}>
+                            {country.name.common}<button onClick={() => getCountryDetails(country.name.common)}>show</button>
+                            {countryToShow.map(country => {
+                                return (
+                                    <div>
+                                        <h1>{country.name.common}</h1>
+                                        <p>capital {country.capital[0]}</p>
+                                        <p>area {country.area}</p>
+                                        <h2>languages</h2>
+                                        {Object.entries(country.languages).map(([key, value]) => (
+                                            <ul key={key}>{value}</ul>
+                                        ))}
+                                    </div>
+                                );
+                            })}
+                        </ul>
+                    );
+                })}
+            </div>
+        );
+    };
 }
 
 export default Country
