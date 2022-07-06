c30ac665ef2fc59b05d415737df2e31496007dc8
Song Meo <songmeo@pm.me>
2022-07-06 09:55:18 +0300

    yay done 2.13

--- part2/countries/src/Country.js
+++ part2/countries/src/Country.js
@@ -4,16 +4,17 @@ import axios from 'axios'
 const Country = (props) => {
     const countriesLength = props.countriesToShow.length
     const [countryToShow, setCountryToShow] = useState([])
-    const getCountryDetails = (countryToShowName) => {
+    const getCountryDetail = (countryToShowName) => {
         axios
         .get(`https://restcountries.com/v3.1/name/${countryToShowName}`)
-        .then(response => {             
+        .then(response => {
             setCountryToShow(response.data)
         })
     }
-    const showCountryDetail = (country) => {
-        console.log(country)
-        return (
+
+    const CountryDetail = (props) => {
+        const country = props.country
+        return(
             <div>
                 <h1>{country.name.common}</h1>
                 <p>capital {country.capital[0]}</p>
@@ -25,7 +26,17 @@ const Country = (props) => {
             </div>
         )
     }
-    
+
+    const ShowCountryDetail = (props) => {
+        const [show, setShow] = useState(false);
+        if (show) {
+            return <CountryDetail country={props.country} />
+        }
+        else {
+            return <button onClick={() => setShow(true)}>show</button>
+        }
+    }
+
     if (countriesLength > 10) {
         return (
             <p>Too many matches, specify another filter</p>
@@ -33,11 +44,11 @@ const Country = (props) => {
     }
     else if (countriesLength === 1 && countryToShow.length === 0) {
         const countryToShowName = props.countriesToShow[0].name.common
-        return getCountryDetails(countryToShowName)
+        getCountryDetail(countryToShowName)
     }
     else if (countriesLength === 1 && countryToShow.length === 1) {
         const country = countryToShow[0]
-        return showCountryDetail(country)
+        return <CountryDetail country={country} />
     }
     else if (countryToShow.length === 1) {
         setCountryToShow([])
@@ -49,13 +60,13 @@ const Country = (props) => {
                     return (
                         <ul key={country.name.common}>
                             {country.name.common}
-                            <button onClick={() => showCountryDetail(country)}>show</button>
+                            <ShowCountryDetail country={country} />
                         </ul>
                     );
                 })}
             </div>
         );
-    };
+    }
 }
 
 export default Country
