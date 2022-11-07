03174a98680b63072b139a35385082e72d965749
Song Meo <songmeo@k-space.ee>
2022-11-07 09:29:29 +0200

    3.9 change baseUrl for frontend to work with backend

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.use(express.json())
+app.use(cors())
 
 morgan.token('data', function (req, res) {
   if (req.body) {
