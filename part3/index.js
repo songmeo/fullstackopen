35f98e57489534abaad393b11055964a528befff
Song Meo <songmeo@k-space.ee>
2022-11-07 16:50:30 +0200

    frontend build to backend, deploy on fly.io

--- part3/index.js
+++ part3/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 8080
 app.use(express.json())
 app.use(cors())
 
