240b5f627ebd0383b3d848d4f26eaf7732ed4679
Song Meo <songmeo@k-space.ee>
2022-11-09 15:01:30 +0200

    3.12

---
+++ part3/mongo.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.odtsjnj.mongodb.net/?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 3) {
+    mongoose
+        .connect(url)
+        .then((result) => {
+            console.log('connected')
+
+            return Person.find({}).then(result => {
+                result.forEach(person => {
+                console.log(`${person.name} ${person.number}`)
+                })
+            })
+        })
+        .then(() => {
+            return mongoose.connection.close()
+        })
+        .catch((err) => console.log(err))
+} else {
+    mongoose
+    .connect(url)
+    .then((result) => {
+      console.log('connected')
+  
+      const person = new Person({
+        name: name,
+        number: number
+      })
+
+      return person.save()
+    })
+    .then(() => {
+      console.log(`added ${name} number ${number} to phonebook`)
+      return mongoose.connection.close()
+    })
+    .catch((err) => console.log(err))
+}
