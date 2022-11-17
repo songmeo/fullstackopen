e04548894a0771fa1c19a32b2c9f5e68b92924cc
Song Meo <songmeo@pm.me>
2022-11-17 15:47:04 +0200

    4.4 totalLikes

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
     return 1
   }
   
-  module.exports = {
-    dummy
+const totalLikes = (blog_lists) => {
+  const reducer = (a, b) => {
+    return {likes: a.likes + b.likes}
   }
+  return blog_lists.length === 0
+    ? blog_lists[0].likes
+    : blog_lists.reduce(reducer).likes
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}
