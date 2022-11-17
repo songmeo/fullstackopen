a26bf2e9fa8b8060f20471d7c3827810a18f284f
Song Meo <songmeo@pm.me>
2022-11-18 00:03:51 +0200

    4.8 blog test

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('blogs are returned as json', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body).toHaveLength(2)
+}, 100000)
+
+afterAll(() => {
+  mongoose.connection.close()
+})
